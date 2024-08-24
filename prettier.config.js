0e19dc4ab14afde63ca266b3e5105131b8f1ca17
Hunter Jennings <jenningsdhunter@gmail.com>
2024-08-24 09:44:08 -0400

    chore(prettier): add base prettier formatting and config

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: ["prettier-plugin-organize-imports", "prettier-plugin-tailwindcss", "prettier-plugin-jsdoc"],
+  singleQuote: true,
+  trailingComma: "all",
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  printWidth: 100,
+  semi: true,
+  tabWidth: 2,
+  proseWrap: "preserve",
+  arrowParens: "always",
+  quoteProps: "as-needed",
+  bracketSameLine: false,
+};
